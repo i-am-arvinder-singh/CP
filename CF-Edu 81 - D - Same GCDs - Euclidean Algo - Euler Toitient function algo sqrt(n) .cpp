beb7c40465549a29e523ee2f8e7e6a1ea5a625d0
arvinder <arvinderavy@ymail.com>
2020-06-02 23:42:05 +0545

    1 file

---
+++ CF-Edu 81 - D - Same GCDs - Euclidean Algo - Euler Toitient function algo sqrt(n) .cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace std;
+using namespace __gnu_pbds;//which means policy based DS
+
+#define endl            "\n"
+#define int             long long
+#define ff              first
+#define ss              second
+#define fl(i,a,b)       for(int i=a; i<b; i++)
+#define bfl(i,a,b)      for(int i=a-1; i>=b; i--)
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define vt(type)        vector<type>
+#define omniphantom     ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+//Below is implementation of min heap
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pw(b,p)         pow(b,p) + 0.1
+#define ini             const int
+#define sz(v)           ((int)(v).size())
+#define LEFT(n) (2*n)
+#define RIGHT(n) (2*n+1)
+
+const double pi = acos(-1.0);
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+int phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+void solve()
+{
+    int a,m;
+    cin>>a>>m;
+    int k = __gcd(a,m);
+    cout<<phi(m/k)<<endl;
+}
+
+int32_t main()
+{
+	omniphantom
+    w(t) solve();
+	return 0;
+}
