e0d4cf9c5fa62c288918b1f0d041cc7fcdd8810d
arvinder <arvinderavy@ymail.com>
2020-08-02 14:07:37 +0545

    files

---
+++ CF-Edu 92 - E - Calendar Problem Number theory refer contest material.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace std;
+using namespace __gnu_pbds;//which means policy based DS
+
+#define endl            "\n"
+#define int             long long
+#define ff              first
+#define ss              second
+#define fl(i,a,b)       for(int i=(int)a; i<(int)b; i++)
+#define bfl(i,a,b)      for(int i=(int)a-1; i>=(int)b; i--)
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define vt(type)        vector<type>
+#define omniphantom     ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+//Below is implementation of min heap
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pw(b,p)         pow(b,p) + 0.1
+#define ini             const int
+#define sz(v)           ((int)(v).size())
+#define ppii            pair<int,pii>
+
+const double pi = acos(-1.0);
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve()
+{
+    int m,d,w;
+    cin>>m>>d>>w;
+    int g = __gcd(d-1,w);
+    int ww = w/g;
+    int z = min(d,m);
+    int q = z/ww;
+    int r = z%ww;
+    int ans = (q*(q-1)*ww)/2 + q*r;
+    cout<<ans<<endl;
+
+}
+
+int32_t main()
+{
+	omniphantom
+	#if 1
+	w(t)
+	#endif // 0
+    solve();
+	return 0;
+}
